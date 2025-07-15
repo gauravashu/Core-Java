f2299c7ee100ae13c579ded245ecac693f342a74
gauravashu <ashish.gaurav64@gmail.com>
2025-07-15 23:24:18 +0530

    Add files via upload

---
+++ leap year/src/CyclicSort.java
@@ -0,0 +1,7 @@
+public class CyclicSort{
+    public static void main(String[] args) {
+        for (int i = 1; i <=5; ) {
+            System.out.println("hello world");
+        }
+    }
+}
---
+++ leap year/src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 2000;
+        byte b = (byte) a;
+    }
+}
