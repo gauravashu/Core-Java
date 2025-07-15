4e6de8c8e376449dae4d68082211748025566406
gauravashu <ashish.gaurav64@gmail.com>
2025-07-15 23:22:42 +0530

    Add files via upload

---
+++ J34online/src/Day1variables.java
@@ -0,0 +1,8 @@
+public class Day1variables {
+    public static void main(String[] args) {
+        int a = 5;
+        java.lang.String b = "ashish";
+                System.out.println(a);
+        System.out.println(b);
+    }
+}
